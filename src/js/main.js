a12a5f1156d4832ba4c92dec4b7e6639f0624380
podleskatetiana <podleskatetiana@gmail.com>
2020-12-14 22:02:35 +0200

    site-header / add mobile-menu changed last breakpoint to 993px

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
 const burgerBtn = document.querySelector('.burger');
-const menu = document.querySelector('.menu-list');
-burgerBtn.addEventListener('click', () => {
-    burgerBtn.classList.toggle('burger--opened');
-    console.log(menu);
-});
+burgerBtn.addEventListener('click', () =>
+    burgerBtn.classList.toggle('burger--opened'));
