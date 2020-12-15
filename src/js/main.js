be118b77336f8a39a4378409956769a5b1c7a603
podleskatetiana <podleskatetiana@gmail.com>
2020-12-15 21:45:22 +0200

    site-header / change media styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 const burgerBtn = document.querySelector('.burger');
-burgerBtn.addEventListener('click', () =>
-    burgerBtn.classList.toggle('burger--opened'));
+const menu = document.querySelector('.menu');
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('burger--opened');
+    menu.classList.toggle('menu--opened');
+});
